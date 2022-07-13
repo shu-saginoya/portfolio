f453bb2c87c20238aa3ba27c0917f71d4ec4df25
shu-saginoya <saginoya@envi-heron.sakura.ne.jp>
2022-07-13 18:18:11 +0900

    基本的なページ構成の作成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <section>
+      <ul>
+        <li>Front end</li>
+        <li>Private</li>
+      </ul>
+    </section>
   </main>
 </template>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>This is an profile page</h1>
+  </main>
+</template>
