7fb6ec8bcaf2725d49e2948463e8eb24fd425db4
shu.saginoya <shu.saginoya@gmail.com>
2024-07-05 00:04:10 +0900

    Add VueUse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,5 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  modules: ["@vueuse/nuxt"],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { useImage } from "@vueuse/core";
+
+const avatarUrl = "https://place.dog/300/200";
+const { isLoading } = useImage({ src: avatarUrl });
+</script>
+
 <template>
-  <h1 class="text-red-500">Portfolio</h1>
+  <div>
+    <h1 class="text-red-500">Portfolio</h1>
+    <span v-if="isLoading">Loading</span>
+    <img v-else :src="avatarUrl" />
+  </div>
 </template>
