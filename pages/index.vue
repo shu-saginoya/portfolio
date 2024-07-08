b6d368b42f0666c9c4aa72a72572d67801fde17f
shu.saginoya <shu.saginoya@gmail.com>
2024-07-08 23:19:10 +0900

    Update index

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,43 @@
 <script setup lang="ts">
 import type { Blog } from "@/types/blog";
+import type { Category } from "@/types/category";
 
 const { data } = await useMicroCMSGetList<Blog>({
   endpoint: "blogs",
   queries: { orders: "-date", limit: 100 },
 });
+
+const { data: catData } = await useMicroCMSGetList<Category>({
+  endpoint: "categories",
+});
+
+const { hiddenCatList, changeHiddenCatList } = useHiddenCatList();
 </script>
 
 <template>
-  <main>
+  <main class="flex flex-col gap-8 items-center">
     <h1 class="text-center font-sans text-4xl font-semibold">
       Shu Saginoya Portfolio
     </h1>
-    <ul class="mt-16 grid grid-cols-1 gap-8">
-      <li v-for="blog in data?.contents" :key="blog.id">
+    <ul class="flex gap-2">
+      <li v-for="cat in catData?.contents" :key="cat.id">
+        <button
+          class="inline-block rounded bg-indigo-600 px-1.5 py-0.5 text-sm font-semibold text-white"
+          :class="{
+            'opacity-50': hiddenCatList.includes(cat.id),
+          }"
+          @click="changeHiddenCatList(cat.id)"
+        >
+          {{ cat.name }}
+        </button>
+      </li>
+    </ul>
+    <ul class="grid grid-cols-1 gap-8">
+      <li
+        v-for="blog in data?.contents"
+        v-show="!hiddenCatList.includes(blog.category.id)"
+        :key="blog.id"
+      >
         <NuxtLink
           :to="`/${blog.id}`"
           class="flex flex-col gap-4 sm:transition-shadow sm:hover:shadow md:flex-row md:items-center lg:gap-6"
